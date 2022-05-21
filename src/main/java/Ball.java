8ed276c953e3b2d3a8b0797cecbb8fcad8db6ab4
2pacalypse <murattopak01@gmail.com>
2022-05-22 00:33:02 +0200

    start ball

---
+++ src/main/java/Ball.java
@@ -0,0 +1,103 @@
+package main.java;
+
+import java.awt.Color;
+
+
+import javax.swing.JLabel;
+
+public class Ball{
+	
+	public static final int startX = 300;
+	public static final int startY = 300;
+	public static final int startR = 25;
+	
+	public static final int startVelocityX = 2;
+	public static final int startVelocityY = 2;
+	
+	private int currentX;
+	private int currentY;
+	private int currentR;
+	
+	private int currentVelocityX;
+	private int currentVelocityY;
+	
+	private JLabel ball;
+	
+	Ball(){
+		setCurrentX(startX);
+		setCurrentY(startY);
+		setCurrentR(startR);
+		setCurrentVelocityX(startVelocityX);
+		setCurrentVelocityY(startVelocityY);
+		setBall(new JLabel());
+		getBall().setOpaque(true);
+		getBall().setBounds(getCurrentX(), getCurrentY(), 100, 20);
+		getBall().setBackground(Color.green);
+	}
+	
+
+	public int getCurrentX() {
+		return currentX;
+	}
+
+	public void setCurrentX(int currentX) {
+		this.currentX = currentX;
+	}
+
+	public int getCurrentY() {
+		return currentY;
+	}
+
+	public void setCurrentY(int currentY) {
+		this.currentY = currentY;
+	}
+
+	public int getCurrentR() {
+		return currentR;
+	}
+
+	public void setCurrentR(int currentR) {
+		this.currentR = currentR;
+	}
+
+	public JLabel getBall() {
+		return ball;
+	}
+
+	public void setBall(JLabel ball) {
+		this.ball = ball;
+	}
+	
+	public void updateBall() {
+		getBall().setBounds(getCurrentX(), getCurrentY(), getCurrentR(), getCurrentR());
+	}
+
+
+	public int getCurrentVelocityX() {
+		return currentVelocityX;
+	}
+
+
+	public void setCurrentVelocityX(int currentVelocityX) {
+		this.currentVelocityX = currentVelocityX;
+	}
+
+
+	public int getCurrentVelocityY() {
+		return currentVelocityY;
+	}
+
+
+	public void setCurrentVelocityY(int currentVelocityY) {
+		this.currentVelocityY = currentVelocityY;
+	}
+	
+
+
+
+
+
+
+
+
+}
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,23 +7,27 @@ import java.awt.Dimension;
 import java.awt.event.*;
 
 
-public class Game {
+public class Game implements Runnable{
 	
+	public static final String windowTitle = "Arkanoid";
 	public static final int boardWidth = 600;
 	public static final int boardHeight = 600;
 	
     JFrame frame;
     Paddle paddle;
+    Ball ball;
 
     public void init(){
-        frame = new JFrame("Arkanoid");
+        frame = new JFrame(windowTitle);
         paddle = new Paddle();
+        ball = new Ball();
 
         frame.getContentPane().setPreferredSize(new Dimension(boardWidth, boardHeight));
         frame.pack();
         
         frame.setLayout(null);
         frame.add(paddle.getBar());
+        frame.add(ball.getBall());
         frame.setVisible(true);
 
 
@@ -51,7 +55,32 @@ public class Game {
     public static void main(String[] args){
         Game game = new Game();
         game.init();
-
+        game.run();
     }
+
+
+	@Override
+	public void run() {
+
+			while (true) {
+				ball.setCurrentX((ball.getCurrentX() + ball.getCurrentVelocityX()));
+				ball.setCurrentY((ball.getCurrentY() + ball.getCurrentVelocityY()));
+				ball.updateBall();
+				
+				if (ball.getCurrentX() >= 600 - ball.getCurrentR()) {
+					ball.setCurrentVelocityX(-ball.getCurrentVelocityX());
+					ball.setCurrentVelocityY(ball.getCurrentVelocityY());
+				}
+				
+				
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		
+	}
     
 }
