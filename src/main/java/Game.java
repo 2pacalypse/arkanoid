6868d6535f230995abd6ec3aa34a9f19d7531dc5
2pacalypse <murattopak01@gmail.com>
2022-05-29 14:15:31 +0200

    write readme

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -69,7 +69,7 @@ public class Game {
 	
 	public static final String EXIT_TEXT = "Are you sure you want to exit the game?";
 	public static final String EXIT_TEXT_TITLE = "Quit";
-	public static final String HELP_TEXT = "<html><ul><li>Yellow Bricks: 3 hitsç</li><li>Orange Bricks: 2 hits.</li><li>Red Bricks: 1 hit.</li><li>Move either with mouse or left and right arrow keys.</li> <li>Get back to home with backspace key.</li><li>Only top 10 scores are saved.</li></ul></html>";
+	public static final String HELP_TEXT = "<html><ul><li>Yellow Bricks: 3 hits.</li><li>Orange Bricks: 2 hits.</li><li>Red Bricks: 1 hit.</li><li>Move either with mouse or left and right arrow keys.</li> <li>Get back to home with backspace key.</li><li>Only top 10 scores are saved.</li></ul></html>";
 	public static final String HELP_TEXT_TITLE = "Help";
 	public static final String ABOUT_TEXT = "ABOUT_TEXT";
 	public static final String ABOUT_TEXT_TITLE = "About";
